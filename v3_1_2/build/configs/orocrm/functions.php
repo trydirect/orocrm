4f379a82b16c7888a7a41bce9880f5feb45b5aba
Dziubin Anton <a.dziubin@vr-dev.team>
2019-09-10 08:41:23 +0300

    final version orocrm Dont Work

---
+++ v3_1_2/build/configs/orocrm/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!function_exists('pcntl_sigtimedwait')) {
+    function pcntl_sigtimedwait_new(array $signals, array &$siginfo = array(), $sec = 0, $nano = 0)
+    {
+        pcntl_signal_dispatch();
+
+        if (time_nanosleep($sec, $nano) === true) {
+
+            return false;
+        }
+
+        pcntl_signal_dispatch();
+
+        return true;
+    }
+}
